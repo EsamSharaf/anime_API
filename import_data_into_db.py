c9e4155e6e023df6805bd901f61ac34f17fe89de
EsamSharaf <asoomsharaf@hotmail.com>
2023-10-26 21:08:04 +0200

    fix: changes after a review

--- import_data_into_db.py
+++ import_data_into_db.py
@@ -8,27 +8,17 @@ Session = sessionmaker(bind=engine)
 
 session = Session()
 
-empty_field_val = {
-    'anime_id': 'unkownn',
-    'name': 'unkown',
-    'genre': 'unkown',
-    'type': 'unkown',
-    'episodes': -1,
-    'rating': -1,
-    'members': -1,
-}
-
 with open('animes.csv', newline='') as csvfile:
 
     # Read csv file
     anime_table = csv.DictReader(csvfile)
 
     for row in anime_table:
-        for col in row:
-            if row[col] == "":
 
-                row[col] = empty_field_val[col]
+        try:
+            row['rating'] = float(row['rating'])
+        except ValueError:
+            row['rating'] = 0.0
 
         session.add(Anime(**row))
-
         session.commit()
