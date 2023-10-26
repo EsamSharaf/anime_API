93810a9e006a72d724dd9669dbefdd33fbb02995
ESAM SHARAF <EsamSharaf@users.noreply.github.com>
2023-10-26 21:18:56 +0200

    Anime API Task 0 (#1)
    feat: Import data into db

---
+++ import_data_into_db.py
@@ -0,0 +1,24 @@
+import csv
+
+from sqlalchemy.orm import sessionmaker
+
+from models import Anime, engine
+
+Session = sessionmaker(bind=engine)
+
+session = Session()
+
+with open('animes.csv', newline='') as csvfile:
+
+    # Read csv file
+    anime_table = csv.DictReader(csvfile)
+
+    for row in anime_table:
+
+        try:
+            row['rating'] = float(row['rating'])
+        except ValueError:
+            row['rating'] = 0.0
+
+        session.add(Anime(**row))
+        session.commit()
---
+++ models.py
@@ -0,0 +1,51 @@
+from sqlalchemy import Column, Float, Integer, String, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class Anime(Base):
+    """Model class returns a table object of a name animes
+
+    :param anime_id: Anime ID
+    :type anime_id: int
+    :param name: Anime name
+    :type name: str
+    :param genre: Anime genre
+    :type genre: str
+    :param type: Anime type
+    :type type: str
+    :param episodes: Episode number
+    :type episodes: int
+    :param rating: Episode rating
+    :type rating: float
+    :param members: Anime members
+    :type members: int
+    """
+
+    __tablename__ = "animes"
+
+    anime_id = Column("anime_id", Integer, primary_key=True)
+    name = Column("name", String)
+    genre = Column("genre", String)
+    type = Column("type", String)
+    episodes = Column("episodes", Integer)
+    rating = Column("rating", Float)
+    members = Column("members", Integer)
+
+    def __init__(self, anime_id, name, genre, type, episodes, rating, members):
+        """
+        Constructor method
+        """
+        self.anime_id = anime_id
+        self.name = name
+        self.genre = genre
+        self.type = type
+        self.episodes = episodes
+        self.rating = rating
+        self.members = members
+
+
+engine = create_engine("sqlite:///animeDB")
+
+Base.metadata.create_all(bind=engine)
