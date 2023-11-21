162d6333a9fbef4f5d38d8729d73aad4fd8b514a
EsamSharaf <asoomsharaf@hotmail.com>
2023-11-21 22:09:53 +0100

    refactor: print statement deleted in test_views.py

--- tests/test_views.py
+++ tests/test_views.py
@@ -1,6 +1,5 @@
 def test_animes_route(client):
     with client:
         response = client.get('/api/v1/animes/')
-        print('\n', response.get_data())
         assert response.status_code == 200
         assert response.get_data() == b'[{"anime_id":111222,"episodes":27,"genre":"Horror","members":1256,"name":"anime_1","rating":8.5,"type":"TV"},{"anime_id":11,"episodes":27,"genre":"Action","members":123456,"name":"anime_default","rating":8.0,"type":"TV"}]\n'
