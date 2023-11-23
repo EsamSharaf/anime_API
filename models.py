9e200a91e86c4ce75b9bcdebd31d12553a4e40aa
EsamSharaf <asoomsharaf@hotmail.com>
2023-11-23 23:53:57 +0100

    refactor: import declarative_base in models.py changed following a terminal warning

--- models.py
+++ models.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Column, Float, Integer, String, create_engine
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
 
