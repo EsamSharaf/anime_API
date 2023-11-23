679ca2c7fbd79067e219e04649316488b123b204
EsamSharaf <asoomsharaf@hotmail.com>
2023-11-23 23:27:10 +0100

    test: json form used instead of binary form for assertion the data

--- tests/test_views.py
+++ tests/test_views.py
@@ -1,5 +1,18 @@
+import json
+
+
 def test_animes_route(client):
-    with client:
-        response = client.get('/api/v1/animes/')
-        assert response.status_code == 200
-        assert response.get_data() == b'[{"anime_id":111222,"episodes":27,"genre":"Horror","members":1256,"name":"anime_1","rating":8.5,"type":"TV"},{"anime_id":11,"episodes":27,"genre":"Action","members":123456,"name":"anime_default","rating":8.0,"type":"TV"}]\n'
+
+    response = client.get('/api/v1/animes/')
+    assert response.status_code == 200
+    assert json.loads(response.get_data()) == [
+        {
+            "anime_id": 111222, "episodes": 27, "genre": "Horror",
+            "members": 1256, "name": "anime_1", "rating": 8.5, "type": "TV"
+        },
+        {
+            "anime_id": 11, "episodes": 27, "genre": "Action",
+            "members": 123456, "name": "anime_default", "rating": 8.0,
+            "type": "TV"
+        },
+    ]
