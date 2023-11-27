f1f46acd239dec5a8b22bf2fd0d5af690071e53c
EsamSharaf <asoomsharaf@hotmail.com>
2023-11-27 22:34:00 +0100

    test: test of of the feature added

--- tests/test_views.py
+++ tests/test_views.py
@@ -1,4 +1,6 @@
 import json
+from .factories import AnimeFactory
+from .db_test import db
 
 
 def test_animes_route(client):
@@ -15,3 +17,26 @@ def test_animes_route(client):
             "type": "TV"
         },
     ]
+
+
+def test_anime_by_name_route(client):
+    AnimeFactory(
+        anime_id=1535,
+        name='Death Note',
+        genre='Mystery, Police, Psychological, Supernatural, Thriller',
+        type='TV',
+        episodes='37',
+        rating=8.71,
+        members=1013917,
+    )
+    db.session.commit()
+
+    response = client.get('/api/v1/anime/Death Note')
+    assert response.status_code == 200
+    assert json.loads(response.get_data()) == {
+
+            "anime_id": 1535, "episodes": 37,
+            "genre": "Mystery, Police, Psychological, Supernatural, Thriller",
+            "members": 1013917, "name": "Death Note",
+            "rating": 8.71, "type": "TV"
+    }
