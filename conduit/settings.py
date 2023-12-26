128faf5f3a83b06b1dbec8d51b4d0a983c344686
EsamSharaf <asoomsharaf@hotmail.com>
2023-12-26 23:54:12 +0100

    style: comment lines in conduit/settings.py deleted

--- conduit/settings.py
+++ conduit/settings.py
@@ -8,12 +8,7 @@ class DevConfig(Config):
 
     ENV = 'dev'
     DEBUG = True
-    # DB_NAME = 'dev.db'
-    # Put the db file in project root
-    # DB_PATH = os.path.join(Config.PROJECT_ROOT, DB_NAME)
     SQLALCHEMY_DATABASE_URI = "sqlite:///animeDB"
-    # CACHE_TYPE = 'simple'  # Can be "memcached", "redis", etc.
-    # JWT_ACCESS_TOKEN_EXPIRES = timedelta(10 ** 6)
 
 
 class TestConfig(Config):
@@ -22,5 +17,3 @@ class TestConfig(Config):
     TESTING = True
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = "sqlite:///animeDB_test.db"
-    # For faster tests; needs at least 4 to avoid "ValueError: Invalid rounds"
-    # BCRYPT_LOG_ROUNDS = 4
