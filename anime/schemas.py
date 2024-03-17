858604bd05a14e86a1b4fd3a321c17ecbc148097
EsamSharaf <asoomsharaf@hotmail.com>
2024-03-17 02:40:19 +0100

    refactor: error messages for AnimeSchema removed . Defaults messages are sufficient.

--- anime/schemas.py
+++ anime/schemas.py
@@ -2,12 +2,12 @@ from marshmallow import Schema, fields
 
 AnimeSchema = Schema.from_dict(
     {
-        "anime_id": fields.Int(required=True, error_messages={"required": "anime_id is required."}),
-        "name": fields.Str(required=True, error_messages={"required": "name is required."}),
-        "genre": fields.Str(required=True, error_messages={"required": "genre is required."}),
-        "type": fields.Str(required=True, error_messages={"required": "type is required."}),
-        "episodes": fields.Int(required=True, error_messages={"required": "episodes is required."}),
-        "rating": fields.Float(required=True, error_messages={"required": "rating is required."}),
-        "members": fields.Int(required=True, error_messages={"required": "members is required."}),
+        "anime_id": fields.Int(required=True),
+        "name": fields.Str(required=True),
+        "genre": fields.Str(required=True),
+        "type": fields.Str(required=True),
+        "episodes": fields.Int(required=True),
+        "rating": fields.Float(required=True),
+        "members": fields.Int(required=True)
     }
 )
